2595199a99f549146f0789f08599cd280f712ec7
frannuca <frannuca@gmail.com>
2016-07-21 17:59:38 +0200

    first commit

---
+++ numericsbase/FrameBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+namespace numericsbase
+{
+    class FrameBase<TIndex,TData> where TIndex:IComparable
+    {
+        
+        public FrameBase(List<string> columns)
+        {
+            tdata.Columns.Add("index", typeof(TIndex));
+
+            foreach (var c in columns)
+                tdata.Columns.Add(c, typeof(TData));
+        }
+
+        public FrameBase(FrameBase<TIndex,TData> that)
+        {
+            tdata = that.tdata.Copy();            
+        }
+        public TData this[TIndex i,string column]
+        {
+            get
+            {
+                return
+                    (from r in tdata.Rows.Cast<DataRow>() where r.Field<TIndex>("index").Equals(i) select r.Field<TData>(column))
+                    .ToList<TData>().FirstOrDefault<TData>();
+            }
+            set
+            {
+
+                var row = (from r in tdata.Rows.Cast<DataRow>()
+                          where r.Field<TIndex>("index").Equals(i)
+                          select r).ToList<DataRow>();
+
+                row.First()[column] = value;                
+            }
+        }
+        DataRow newrow()
+        {
+            return tdata.NewRow();
+        }
+        
+        protected DataTable tdata;        
+    }
+}
---
+++ numericsbase/montecarlo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numericsbase
+{
+    public class montecarlo
+    {
+        public void buildsimucube(ulong dim1, ulong dim2,ulong dim3)
+        {
+
+        }
+
+        
+    }
+}
