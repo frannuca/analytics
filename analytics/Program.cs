8184123ff0d20105b3096074a95f31502c30f6b1
frannuca <frannuca@gmail.com>
2016-10-12 23:46:52 +0200

    implementation of equals for simulation cube class

--- analytics/Program.cs
+++ analytics/Program.cs
@@ -3,29 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using numericsbase;
+using numericsbase.montecarlo;
 using MathNet.Numerics.LinearAlgebra;
 
 namespace Analytics
 {
     class Program
     {
-        static int nsims = 30000;
-        static int nassets = 10;
-        static int ntimes = 250 * 10;
+        static UInt64 nsims = 1000000;
+        static UInt64 nassets = 5;
+        static UInt64 ntimes = 260;
        
       
         static void Main(string[] args)
         {
-            var scube = new simcube(nassets, nsims, ntimes);
 
-            var builder = new montecarloBuild()
-                .withSimulationCube(scube)
-                .withcorrelationMatrix(Matrix<float>.Build.DiagonalIdentity(nassets))
-                ;
-            for (int i = 0; i < nassets; ++i) builder.withdrift(i, 0.05f).withvolatility(i, 0.15f);
+            double szsim = nsims * nassets * ntimes * sizeof(double)/1024/1024;//MB
 
-            var montecarloengine = builder.build();
+
+            long s1 = GC.GetTotalMemory(false);
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            Console.WriteLine(String.Format("Creating first simulation cube with dimension {0} MB, {1} number of samples", szsim,nsims*nassets*ntimes));
+            var scube1 = new SimulationCube(nsims,nassets,ntimes);
+            long s2 = GC.GetTotalMemory(false);
+            Console.WriteLine(String.Format("Creation lasted {0}ms", watch.ElapsedMilliseconds));
+
+            watch = System.Diagnostics.Stopwatch.StartNew();
+
+            Console.WriteLine(String.Format("Creating second simulation cube with dimension {0} MB, {1} number of samples", szsim,  nsims * nassets * ntimes));
+            var scube2 = new SimulationCube(nsims, nassets, ntimes);
+            Console.WriteLine(String.Format("Creation lasted {0}ms", watch.ElapsedMilliseconds));
+
+            Console.WriteLine("Object memory footprints cube1={0} MB", (s2-s1)/1024/1024);
+
+            Console.WriteLine("Checking values");
+            bool equal = scube1 == scube2;
+            if (!equal)
+                Console.WriteLine("Objects are different");
+            else
+                Console.WriteLine("Objects are the same");
         }
     }
 }
---
+++ numericsbase/montecarlo/SimulationCube.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.Distributions;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace numericsbase.montecarlo
+{
+    /// <summary>
+    ///  Use for generating a read only simulation cube of uniformly random distributed numbers
+    /// </summary>
+    public class SimulationCube
+    {
+
+        public enum DISTRIBUTION { UNIFORM=0,NORMAL}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nsims"> total number of simulations to be generated</param>
+        /// <param name="dimension"> Dimensionality of the random vectos to be used</param>
+        /// <param name="nsteps">Number of samples per dimension that need to be generated per simulation</param>       
+
+        public SimulationCube(UInt64 nsims,UInt64 dimension,UInt64 nsteps, DISTRIBUTION distribution=DISTRIBUTION.NORMAL)
+        {
+            number_of_sims = nsims;
+            number_of_dims = dimension;
+            number_of_steps = nsteps;
+            simulation_vector_length = number_of_dims * number_of_steps;
+            
+            GenerateUCube(distribution);
+            
+        }
+
+        readonly public UInt64 number_of_sims;
+        readonly public UInt64 number_of_dims;
+        readonly public UInt64 number_of_steps;
+        readonly public UInt64 simulation_vector_length;
+
+        const int seed = 42;
+
+        private void GenerateUCube(DISTRIBUTION d)
+        {
+            if (array != null) return;
+            array = new double[number_of_sims][][];
+
+            var uniform = new MathNet.Numerics.Random.MersenneTwister(seed);
+            var normal = new MathNet.Numerics.Distributions.Normal(0, 1,uniform);
+
+            Func<double[]> rgn = null;
+            switch(d)
+            {
+                case DISTRIBUTION.UNIFORM:
+                    rgn = () =>
+                    {
+                        return uniform.NextDoubles((int)number_of_steps);
+                    };
+                    break;
+                case DISTRIBUTION.NORMAL:
+                    rgn = () =>
+                    {
+                        double[] r = new double[number_of_steps];
+                        normal.Samples(r);
+                        return r;
+                    };
+                    break;
+
+            }           
+
+            for(UInt64 i = 0; i < number_of_sims; ++i)
+            {
+                array[i] = new double[number_of_dims][];
+                for (UInt64 j = 0; j < number_of_dims; ++j)
+                {
+                    array[i][j] = rgn();
+                }
+            }
+
+        }
+
+
+        public void CorrelateSimulationCube(MathNet.Numerics.LinearAlgebra.Matrix<double> Rho)
+        {
+            var cholesky = Rho.Cholesky().Factor;
+
+
+        }
+
+        public double[] this[UInt64 simulation,UInt64 dimension]
+        {
+            get
+            {                                         
+                return array[simulation][dimension];                                   
+            }
+            private set { }
+        }
+
+        public double[][] this[UInt64 simulation]
+        {
+            get
+            {               
+                return array[simulation];
+            }
+            private set { }
+        }
+
+        public double this[UInt64 simulation,UInt64 dimension,UInt64 step]
+        {
+            get
+            {
+                return array[simulation][dimension][step];
+            }
+            private set { }
+        }
+
+
+        public override bool Equals(object Obj)
+        {
+            SimulationCube other = (SimulationCube)Obj;
+            if(other == null)
+            {
+                return false;
+            }
+            else if(other.number_of_dims != number_of_dims || other.number_of_sims != number_of_sims || other.number_of_steps != number_of_steps)
+            {                
+               return false;                
+            }
+            else
+            {
+                for(UInt64 i = 0; i < number_of_sims; ++i)
+                {
+                    for(UInt64 j = 0; j < number_of_dims; ++j)
+                    {
+                        for(UInt64 k = 0; k < number_of_steps; ++k)
+                        {
+                            if(array[i][j][k] != other[i, j, k])
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator!=(SimulationCube person1, SimulationCube person2)
+        {
+            return !person1.Equals(person2);
+        }
+
+
+        public static bool operator==(SimulationCube cube1, SimulationCube cube2)
+        {
+            if (Object.ReferenceEquals(cube1,null))
+            {
+                return Object.ReferenceEquals(cube2,null)? true:false;
+                
+            }
+            else
+            {
+                return cube1.Equals(cube2);
+            }
+            
+        }
+        private double[][][] array = null;
+    }
+}
